de4f61e218723a6f4ce2868641e41d6c303e6ce4
firazputraa <mfirazpsa.2004@gmail..com>
2025-07-09 11:05:36 +0700

    first deploy

--- src/components/molecules/ProjectCard.jsx
+++ src/components/molecules/ProjectCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const ProjectCard = ({ image, title}) => {
   return (
-    <Link to={"/detail"} className='hover:scale-105 transition-transform duration-300 ease-in-out'>
+    <Link to={"/"} className='hover:scale-105 transition-transform duration-300 ease-in-out'>
       {/* Perubahan di sini: bg-white/90 atau bg-blue-50/80, dan text-sky-900 untuk judul */}
       <div className="max-w-sm border rounded-lg border-sky-50 shadow-2xl h-full bg-sky-100/20 backdrop-blur-xs">
         <div className="p-1">
